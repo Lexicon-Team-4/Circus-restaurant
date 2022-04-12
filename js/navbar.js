bf1bba1d0a27d1d02693bdfec62718c350a4443d
bojan-vujic <bojan.vujic@yahoo.com>
2022-04-12 09:28:12 +0200

    Updated js file

--- js/navbar.js
+++ js/navbar.js
@@ -5,8 +5,8 @@ const bar = document.getElementsByClassName('bar')
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
-  bar[0].classList.toggle('activeTop')
-  bar[1].classList.toggle('activeMiddle')
-  bar[2].classList.toggle('activeBottom')
+  bar[0].classList.toggle('active-top')
+  bar[1].classList.toggle('active-middle')
+  bar[2].classList.toggle('active-bottom')
 })
 

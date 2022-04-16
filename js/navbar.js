5cfc15390d6da9886ae7b7dc514180132a9582d0
bojan-vujic <bojan.vujic@yahoo.com>
2022-04-17 00:43:28 +0200

    Updated js/ directory.

--- js/navbar.js
+++ js/navbar.js
@@ -1,12 +1,33 @@
-// get the variables from the main document
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks  = document.getElementsByClassName('navbar-links')[0]
-const bar = document.getElementsByClassName('bar')
 
+// EVENT LISTENER TO TOGGLE HAMBURGER MENU BUTTON
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// get the variables from the main document
+var toggleButton = document.getElementsByClassName('toggle-button')[0]
+var navbarLinks  = document.getElementsByClassName('navbar-links')[0]
+var navbarMenu   = document.getElementsByClassName('navbar')[0]
+var bar          = document.getElementsByClassName('bar')
+
+// attatch an event listener to the given class
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
   bar[0].classList.toggle('active-top')
   bar[1].classList.toggle('active-middle')
   bar[2].classList.toggle('active-bottom')
+  navbarMenu.classList.toggle('navbar-active')
 })
+
+
+
+
+// RESTAURANT LOGO HOVER
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// Change restaurant logo image on hover
+$('.logo-img').on('mouseover', function() {
+  $(this).attr('src', 'images/peeka-boo-yellow.png');
+})
+
+$('.logo-img').on("mouseleave", function () {
+  $(this).attr('src', 'images/peeka-boo-logo.png');
+});
+
 
